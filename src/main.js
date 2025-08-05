a1bc4186dde53d214c2de06cada49bf47dd691a2
JBS970 <140853801+JBS970@users.noreply.github.com>
2025-08-05 13:57:30 -0300

    Mudança no vite-config 2

--- src/main.js
+++ src/main.js
@@ -16,8 +16,23 @@ import { createApp } from 'vue'
 // Styles
 import 'unfonts.css'
 
+import { registerSW } from 'virtual:pwa-register'//=============
+
 const app = createApp(App)
 
 registerPlugins(app)
 
 app.mount('#app')
+
+
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm('Nova versão disponível. Recarregar?')) {
+            updateSW()
+        }
+    },
+    onOfflineReady() {
+        console.log('App pronto para uso offline')
+    }
+})
